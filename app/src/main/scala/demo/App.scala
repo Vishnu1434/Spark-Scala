7021378cb90f4a27e24bdd05533e65c97e23448c
Vishnu Teja Gunji <vishnu.pandu9848@gmail.com>
2025-08-16 10:36:08 +0530

    new changes

---
+++ app/src/main/scala/demo/App.scala
@@ -0,0 +1,23 @@
+package demo
+
+import org.apache.spark.sql.SparkSession
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Hello from App")
+
+    val spark = SparkSession.builder().appName("spark-scala").master("local[*]").getOrCreate()
+
+    val df = spark.emptyDataFrame
+
+    df.createOrReplaceTempView("df")
+
+    val sqlDF = spark.sql("select * from df")
+
+    sqlDF.show(false)
+
+    Thread.sleep(5000)
+    println("------------------THE END--------------------")
+    System.exit(0)
+  }
+}
